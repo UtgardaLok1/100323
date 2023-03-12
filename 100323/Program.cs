20c19a77576d2a4a20ba639d41d0fd02d35b160e
Loki <Loki@DESKTOP-EBNIE27>
2023-03-12 11:55:58 +0700

    Добавьте файлы проекта.

---
+++ 100323/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ваше имя? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здорова, " + name + "!");
+        }
+    }
+}
